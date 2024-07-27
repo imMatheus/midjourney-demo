78d561c40797826b0d80b5863067595816b10961
imMatheus <matheusmendesbarata03@gmail.com>
2024-07-27 16:03:15 +0200

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import { useState } from 'react'
+import { Grid } from './components/Grid'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="max-w-6xl mx-auto">
+        <Grid />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
